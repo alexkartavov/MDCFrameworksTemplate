a9db65043a908ec4d4f75b52cb5452d9d0c135c4
Alexander Kartavov <alexanderk@outlook.com>
2018-07-26 11:19:23 -0400

    fixed casing on a library which was crashing the app

--- fat.sh
+++ fat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Create fat binaries from the [built frameworks](#Framework dependencies)
 
-for f in MaterialComponents MotionInterChange MotionAnimator MotionTransitioning MDFInternationalization MDFTextAccessibility
+for f in MaterialComponents MotionInterchange MotionAnimator MotionTransitioning MDFInternationalization MDFTextAccessibility
     do
         # Copy the required headers and other files.
         cp -pr ./build/Debug-iphoneos/${f}/${f}.framework Frameworks/
